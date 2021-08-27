888607b9c30d2dce6b976551173a3004225b6bfc
csprl-nowigence.com <caleb.spraul@nowigence.com>
2021-08-27 09:25:16 -0400

    add class instances to loader.py for simple testing

--- loader.py
+++ loader.py
@@ -7,3 +7,23 @@ load_dotenv()
 ATLAS_API_PUBLIC_KEY = os.getenv("ATLAS_API_PUBLIC_KEY")
 ATLAS_API_PRIVATE_KEY = os.getenv("ATLAS_API_PRIVATE_KEY")
 ATLAS_GROUP_ID = os.getenv("ATLAS_GROUP_ID")
+ATLAS_ORG_ID = os.getenv("ATLAS_ORG_ID")
+ATLAS_CLUSTER_NAME = os.getenv("ATLAS_CLUSTER_NAME")
+
+from atlas_search import AtlasSearchClient
+asc = AtlasSearchClient(ATLAS_API_PUBLIC_KEY, ATLAS_API_PRIVATE_KEY, ATLAS_GROUP_ID, ATLAS_CLUSTER_NAME)
+
+from access_list import AccessListClient
+alc = AccessListClient(ATLAS_API_PUBLIC_KEY, ATLAS_API_PRIVATE_KEY, ATLAS_GROUP_ID)
+
+from alerts import AlertsClient
+ac = AlertsClient(ATLAS_API_PUBLIC_KEY, ATLAS_API_PRIVATE_KEY, ATLAS_GROUP_ID)
+
+from data_lake import DataLakeClient
+dlc = DataLakeClient(ATLAS_API_PUBLIC_KEY, ATLAS_API_PRIVATE_KEY, ATLAS_GROUP_ID)
+
+from invoices import InvoicesClient
+ic = InvoicesClient(ATLAS_API_PUBLIC_KEY, ATLAS_API_PRIVATE_KEY, ATLAS_ORG_ID)
+
+from root import RootClient
+rc = RootClient(ATLAS_API_PUBLIC_KEY, ATLAS_API_PRIVATE_KEY)
